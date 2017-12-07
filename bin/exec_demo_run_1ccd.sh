d925e8029d4d1eafb4599d72d0f8bcec92d8fe52
Eric Bellm <ecbellm@uw.edu>
2017-12-07 16:06:18 -0600

    add single visit-ccd runner

---
+++ bin/exec_demo_run_1ccd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /software/lsstsw/stack/loadLSST.bash
+setup lsst_distrib
+setup obs_decam
+
+AP_VERIFY_HITS2015_DIR=/project/krzys001/ap_verify_hits2015
+setup -k -r $AP_VERIFY_HITS2015_DIR
+
+
+BASE_DIR=./
+setup -k -r $BASE_DIR/ap_association
+setup -k -r $BASE_DIR/ap_pipe
+setup -k -r $BASE_DIR/ap_verify
+
+
+
+ap_verify.py --dataset HiTS2015 --output ap_verify_HiTS --dataIdString "visit=${1} ccdnum=${2} filter=g" --silent;
