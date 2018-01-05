8b6d490f95fd5dcafd63a379c7f31c62d77716f6
Krzysztof Findeisen <kfindeis@uw.edu>
2018-01-05 11:32:10 -0800

    Replace profiling test case.
    Dummy task runs are too fast to give good results, but IsrTask is too
    complex to run on the fly. I've replaced it with FringeTask, which
    can be run with only minimal setup.

--- tests/test_profiling.py
+++ tests/test_profiling.py
@@ -24,23 +24,63 @@ from __future__ import absolute_import, division, print_function
 
 import unittest
 
+import numpy as np
 import astropy.units as u
 
-from lsst.ip.isr import IsrTask
 import lsst.utils.tests
-from lsst.verify import Measurement
+import lsst.afw.image as afwImage
+from lsst.ip.isr import FringeTask
+import lsst.verify
 from lsst.ap.verify.measurements.profiling import measureRuntime
+
+
+def _createFringe(width, height, filterName):
+    """Create a fringe frame
+
+    Parameters
+    ----------
+    width, height: `int`
+        Size of image
+    filterName: `str`
+        name of the filterName to use
+
+    Returns
+    -------
+    fringe: `lsst.afw.image.ExposureF`
+        Fringe frame
+    """
+    image = afwImage.ImageF(width, height)
+    array = image.getArray()
+    freq = np.pi / 10.0
+    x, y = np.indices(array.shape)
+    array[x, y] = np.sin(freq * x) + np.sin(freq * y)
+    exp = afwImage.makeExposure(afwImage.makeMaskedImage(image))
+    exp.setFilter(afwImage.Filter(filterName))
+    return exp
 
 
 class MeasureRuntimeTestSuite(lsst.utils.tests.TestCase):
 
     def setUp(self):
-        self.task = IsrTask()
-        try:
-            self.task.run(ccdExposure=None)
-        except AttributeError:
-            # I wanted the run to fail...
-            pass
+        """Run a dummy instance of `FringeTask` so that test cases can measure it.
+        """
+        # Create dummy filter and fringe so that `FringeTask` has short but
+        # significant run time.
+        # Code adapted from lsst.ip.isr.test_fringes
+        size = 128
+        dummyFilter = 'FILTER'
+        afwImage.utils.defineFilter(dummyFilter, lambdaEff=0)
+        exp = _createFringe(size, size, dummyFilter)
+
+        # Create and run `FringeTask` itself
+        config = FringeTask.ConfigClass()
+        config.filters = [dummyFilter]
+        config.num = 1000
+        config.small = 1
+        config.large = size // 4
+        config.pedestal = False
+        self.task = FringeTask(name="fringe", config=config)
+        self.task.run(exp, exp)
 
     def tearDown(self):
         del self.task
@@ -48,24 +88,24 @@ class MeasureRuntimeTestSuite(lsst.utils.tests.TestCase):
     def testValid(self):
         """Verify that timing information can be recovered.
         """
-        meas = measureRuntime(self.task.getFullMetadata(), taskName='isr', metricName='ip_isr.IsrTime')
-        self.assertIsInstance(meas, Measurement)
+        meas = measureRuntime(self.task.getFullMetadata(), taskName='fringe', metricName='ip_isr.IsrTime')
+        self.assertIsInstance(meas, lsst.verify.Measurement)
         self.assertEqual(meas.metric_name, lsst.verify.Name(metric='ip_isr.IsrTime'))
         self.assertGreater(meas.quantity, 0.0 * u.second)
-        # The Task didn't actually do anything, so it should be short
-        self.assertLess(meas.quantity, 1.0 * u.second)
+        # Task normally takes 0.2 s, so this should be a safe margin of error
+        self.assertLess(meas.quantity, 10.0 * u.second)
 
     def testNoMetric(self):
         """Verify that trying to measure a nonexistent metric fails.
         """
         with self.assertRaises(TypeError):
-            measureRuntime(self.task.getFullMetadata(), taskName='isr', metricName='foo.bar.FooBarTime')
+            measureRuntime(self.task.getFullMetadata(), taskName='fringe', metricName='foo.bar.FooBarTime')
 
     def testNotRun(self):
         """Verify that trying to measure a real but inapplicable metric returns None.
         """
-        notRun = IsrTask(IsrTask.ConfigClass())
-        meas = measureRuntime(notRun.getFullMetadata(), taskName='isr', metricName='ip_isr.IsrTime')
+        notRun = FringeTask(name="fringe", config=FringeTask.ConfigClass())
+        meas = measureRuntime(notRun.getFullMetadata(), taskName='fringe', metricName='ip_isr.IsrTime')
         self.assertIsNone(meas)
 
 
