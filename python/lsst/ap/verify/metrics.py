e03b59553c98c0866d2b4c9ac50ebe5410afc573
Krzysztof Findeisen <kfindeis@uw.edu>
2019-10-14 15:15:09 -0700

    Remove unused DeprecatedAction.

--- python/lsst/ap/verify/metrics.py
+++ python/lsst/ap/verify/metrics.py
@@ -34,7 +34,6 @@ import argparse
 import collections
 import copy
 import os
-import warnings
 
 import lsst.utils
 from lsst.verify.gen2tasks import MetricsControllerTask
@@ -62,30 +61,6 @@ class MetricsParser(argparse.ArgumentParser):
         self.add_argument('--image-metrics-config',
                           help='The config file specifying the image-level metrics to measure. '
                                'Defaults to config/default_image_metrics.py.')
-
-
-class DeprecatedAction(argparse.Action):
-    """An `argparse.Action` that stores nothing and issues a `FutureWarning`.
-
-    Parameters
-    ----------
-    args
-        Positional arguments to `argparse.Action`.
-    deprecationReason : `str`
-        A mandatory keyword argument to `argparse.ArgumentParser.add_argument`
-        that describes why the argument was deprecated. The explanation will be
-        printed if the argument is used.
-    kwargs
-        Keyword arguments to `argparse.Action`.
-    """
-    def __init__(self, *args, deprecationReason, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.reason = deprecationReason
-
-    def __call__(self, _parser, _namespace, _values, option_string=None):
-        message = "%s has been deprecated, because %s. It will be removed in a future version." \
-            % (option_string, self.reason)
-        warnings.warn(message, category=FutureWarning)
 
 
 class _OptionalFormatDict(collections.UserDict):
