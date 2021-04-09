88aea00cc0584dceaaa6cdcb42aed5eed20ee82d
Krzysztof Findeisen <kfindeis@uw.edu>
2021-04-09 17:02:57 -0500

    Add __all__ to modules.

--- python/lsst/ap/verify/dataset.py
+++ python/lsst/ap/verify/dataset.py
@@ -21,6 +21,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+__all__ = ["Dataset"]
+
 import os
 import warnings
 
--- python/lsst/ap/verify/workspace.py
+++ python/lsst/ap/verify/workspace.py
@@ -21,6 +21,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+__all__ = ["Workspace", "WorkspaceGen2", "WorkspaceGen3"]
+
 import abc
 import os
 import pathlib
